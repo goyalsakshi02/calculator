1691f64f112f9ae85e3eef74d7fd395cfde579bd
Sakshi Goyal <sakshi.goyal@dianapps.com>
2022-04-22 17:41:16 +0530

    update js file

--- calFunct.js
+++ calFunct.js
@@ -1,68 +1,97 @@
-let  operands = [];
-let operators = [];
-
 function num(val) {
-  document.getElementById("gout").innerHTML += val;
+  document.getElementById("calculatorInput").value += val;
+}
 
 function add() {
-  document.getElementById("gout").innerHTML += '+';
+  document.getElementById("calculatorInput").value += '+';
+}
 
 
-function Substract() {
-  document.getElementById("gout").innerHTML += '-';
-
+function subs() {
+  document.getElementById("calculatorInput").value += '-';
+}
 
 function multiply() {
-  document.getElementById("gout").innerHTML += '*';
-
+  document.getElementById("calculatorInput").value += '*';
+}
 function division() {
-  document.getElementById("gout").innerHTML += '/';
+  document.getElementById("calculatorInput").value += '/';
+}
+function clr() {
+  document.getElementById("calculatorInput").value = "" ;
+}
 
 function equals() {
 
-  let data = document.getElementById("gout").innerHTML;
-  
-  for( let i=0; i<data.length; i++){
-    
-    let ch = data[i]
-    
-    if(ch === '+' || ch === '-' || ch === '/' || ch === '*'){
-       while(operators.length()>0){
-          let optr = operators.pop()
-          let val2 = operands.pop()
-          let val1 = operands.pop()
+  let exp = document.getElementById("calculatorInput").value
+  const operation = ["/","*","-","+"];
+  let  num = []
+  const opToPerform = []
+  var lastIndex = -1
 
-          let res = operation(val1, val2, optr)
-          console.log("res",res)
-         }
-      }
-    else{
-      operands.push(parseint(ch))
-    }
-    }
-      
+for (i = 0; i <= exp.length; i++) {
+
+  if (operation.includes(exp[i]) || (i === exp.length)) {
+    
+    let subString = exp.substring(lastIndex + 1, i);
+     
+     num.push(+subString);
+    
+    if(exp[i] !== undefined){
+      num.push(exp[i])
     }
 
-function Operation (a , b , op) {
-  if ( op === '+'){
-    return a+b
-  }
-  else if (op === '-'){
-    return a-b
-  }
-  else if (op === '*') {
-    return a*b
-  }
-  else {
-    return a/b
+      lastIndex = i
+     
   }
 }
 
-function operatorOrders(op) {
-  if (op === '-' && op === '+') {
-    return 1
-  }
-  else {
-    return 2
-  }
+  for(let k=0; k < operation.length; k++ ){
+    
+    
+     tempNum = [];
+    
+    for (i=0; i<num.length; i++) {
+      
+     let store = num[i]
+      
+    let skip = false
+    let currentOp = ''
+      
+      
+     switch (true) {
+       case String(num[i]) == String(operation[k]):
+        let val1 = num[i-1]
+        let val2 = num[i+1]
+        let rVal;
+        if( operation[k] == '/'){
+            rVal = val1 / val2;
+        }else if(operation[k] == '*'){
+          rVal = val1 * val2;
+          
+        } else if(operation[k] == '-'){
+          rVal = val1 - val2;
+          
+        } else if(operation[k] == '+'){
+          rVal = val1 + val2;
+          
+        }
+        
+        tempNum.pop()
+        tempNum.push(rVal)
+        i += 1
+        //skip = true;
+         break ;
+       default :
+        skip = false
+        tempNum.push(store)
+        break;
+      }      
+      //console.log('tempNum', tempNum)
+    }
+    num = tempNum;
+    }
+    
+    
+  document.getElementById("calculatorInput").value = num ;
 }
